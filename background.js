0502b9901111c596449d6a234834758cc9eb8d79
zhao <zhoubk1451@163.com>
2021-11-19 19:21:23 +0800

    first commit

--- background.js
+++ background.js
@@ -1,15 +1,24 @@
+function desktop_notification(msg){
+    chrome.notifications.create("id", { 
+        type : 'basic',
+        title : ' ',  // 这里我故意使显示这个为空，显得没那么拥挤
+        message : msg,
+        iconUrl : 'icon/128.png'
+    });
+}
+
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
        
         // "https://yingxiao.pinduoduo.com/mms-gateway/venus/api/subway/keyword/listKeywordPage",
         if (request.request.request.url == "https://yingxiao.pinduoduo.com/mms-gateway/venus/api/subway/unitBid/list"){
            console.log("检测到"+request.request.request.url);
-           chrome.notifications.create(null, {
-    type: 'basic',
-    iconUrl: 'icon/128.png',
-    title: '监听到了',
-    message: '您刚才点击了自定义右键菜单！'
-});
+           desktop_notification(request.request.request.url);
+           // 通知定时2秒进行清理
+           setTimeout(function(e){
+            chrome.notifications.clear("id");
+            }, 2000);
             console.log(JSON.parse(request.request.response.body.content));
             return true;
         }
